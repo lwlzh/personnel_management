16d31675640d80c3436c693ea3158508a2ec33bc
志皓 卢 <qq931863282@outlook.com>
2022-07-24 10:40:10 +0800

    udpate

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IndexPage></IndexPage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IndexPage from './components/IndexPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    IndexPage
+}
 }
 </script>
 
